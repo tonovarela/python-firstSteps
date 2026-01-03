6a506fb24647170a7875c37b351dab12b50978c2
tonovarela <tonovarela@live.com>
2026-01-03 16:44:21 -0600

    Incorporacion de la seccion de condicionales

---
+++ condicionales/01-if_condicional.py
@@ -0,0 +1,22 @@
+
+
+# is_old = False
+
+# if (is_old):
+#     print("Es viejo")
+# else:
+#     name= "Varela"
+#     print(name)
+#     print("Aqui continua el else")    
+
+# print("Termino")
+
+
+is_student =False
+
+if is_student:
+    print("Tienes descuento de estudiante")
+else:
+    print("Necesitas pagar el boleto normal")    
+
+
---
+++ condicionales/02-elif_conditional.py
@@ -0,0 +1,13 @@
+is_old  = True
+is_licenced = True
+
+if is_old:
+    print("Tienes edad para manejar")
+elif is_licenced:
+    print("Puede manejar con tu licencia en tu ciudad")
+elif True:
+     pass   
+else:
+    print("Espera a cumplir la mayoria de edad")    
+
+
