3376130234b513a48ce8565a5ddc225a7baded81
tonovarela <tonovarela@live.com>
2026-01-06 16:34:52 -0600

    Uso de sets iniciado

---
+++ tuples-sets/01-tuple.py
@@ -0,0 +1,16 @@
+tuple = (1, 2, 3, 4, 5,"Hola",[123,4],4)
+
+
+
+# print(tuple.count(4))
+# print(tuple.index(2))
+# tuple[4] ="Mundo"
+
+
+new_tuple = tuple[4]
+
+print(new_tuple)
+
+week =("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo")
+
+print(week[0:5])
---
+++ tuples-sets/02-set.py
@@ -0,0 +1,16 @@
+my_set = {1, 2, 3, 4, 5,1,2}
+
+# Coleccion desordenada
+# No tiene indices
+# No permite duplicados
+
+print(my_set) 
+
+my_set.add(1)
+print(my_set)
+
+
+username ={ "ricardo123","ricardo123"}
+
+print(username) 
+
