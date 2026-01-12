4a673c9b2c38d47826b6625c02b73b010734dc1c
tonovarela <tonovarela@live.com>
2026-01-12 17:18:27 -0600

    Listo

---
+++ errors-management/01-errors.py
@@ -0,0 +1,23 @@
+
+# x="5"
+# y=5
+# print(x+y)¡
+# int("Holaaa")
+
+# lista = [1,2,3,4]
+# lista[5]
+
+# my_dict = {
+#     "a":1
+# }
+
+# my_dict["b"]
+
+# x=10
+# x.append(4)
+
+
+x= 10 /0
+
+print(x)
+
---
+++ errors-management/02-try_except.py
@@ -0,0 +1,20 @@
+
+def divide_numbers():
+    try:
+        a =int(input("Ingresa el numerador: "))
+        b =int(input("Ingresa el denominador: "))    
+        result = a /b
+        print(result)    
+        return result
+    except ValueError:
+        print("Por favor solo ingresa numeros")    
+    except ZeroDivisionError:
+        print("La division entre 0 no esta definida")
+    except Exception as error:
+        print(type(error)) 
+    finally:
+        print("Me ejecuto")    
+        
+    
+    
+divide_numbers()    
